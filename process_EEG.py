f84b2812b6c8dd13875412b918a1b54c83d01f09
satvik94 <satvikvenkatesh@gmail.com>
2021-04-09 16:19:31 +0530

    Added process_EEG.py

---
+++ process_EEG.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy import signal
+
+from biosppy import storage
+from biosppy.signals.eeg import eeg
+from biosppy.signals.eeg import get_power_features
+
+import threading
+import tkinter as tk
+import time
+
+import os
+import math
+import sys
+
+from numpy import genfromtxt
+
+expt_path = ""
+
+data_file = input("Enter path of data file (Example: data.csv): ")
+
+my_data = genfromtxt(data_file, delimiter=',')
+
+# Declare headset-related variables
+
+
+# Declare variables for alpha BCI. `data_block` stores the most recent data and uses it for analysis.
+sampling_rate = 250
+analysis_block_window = 1.0
+data_block = np.zeros((int(sampling_rate * analysis_block_window), 8))
+alpha_threshold = 0.02
+
+data_2 = np.zeros((1, 9))
+b_notch, a_notch = signal.iirnotch(50.0, 30.0, 250.0)
+
+data_file_alpha = "processed_EEG.csv"
+file_alpha = open(data_file_alpha, "w")
+
+for i in range(0, my_data.shape[0]):
+	data_2[0, 0:8] = my_data[i, 0:8]
+	mind_status_label = my_data[i, 8]
+	data_2[0, 8] = mind_status_label
+
+	# This deletes the oldest EEG sample and inserts the newest sample. In other words, it acts a FIFO queue.
+	data_block = np.roll(data_block, -1, axis=0)
+	data_block[-1, :] = my_data[i, 0:8]
+
+
+	# Code for alpha BCI and data preprocessing.
+
+	# The below if statement discards the first analysis_block_window and checks if data_block is ready for analysis.
+	if i > int(sampling_rate * analysis_block_window) and i % int(sampling_rate * analysis_block_window) == 0:
+	    
+	    # Apply a notch filter to the signal
+	    y_notched = signal.filtfilt(b_notch, a_notch, data_block, axis = 0)
+
+	    # Extract the powers of EEG bands using the 'biosppy.signals.eeg' package
+	    ts, filtered, features_ts, theta, alpha_low, alpha_high, beta, gamma, plf_pairs, plf = eeg(signal=y_notched,
+	        sampling_rate=sampling_rate, show=False)
+
+	    a_low_pow = np.mean(alpha_low[:, :1])
+	    a_high_pow = np.mean(alpha_high[:, :1])
+	    avg_alpha_pow = (a_low_pow + a_high_pow) / 2.0
+
+
+	    mind_status = -1
+
+	    if avg_alpha_pow > alpha_threshold:
+	        mind_status = 1
+	    else:
+	        mind_status = 0
+
+	    data_line = "{:.5f}, {}, {}\n".format(avg_alpha_pow, mind_status, mind_status_label)
+
+	    file_alpha.write(data_line)
